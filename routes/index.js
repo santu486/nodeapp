74e5ccea9778f31bccc69f3bafae257a78c1b095
santu486 <39703300+santu486@users.noreply.github.com>
2024-11-16 12:36:07 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express from santoh' });
+  res.render('index', { title: 'Express from santoh updated hereeeeeeeeeeeeeeee66666666666' });
 });
 
 module.exports = router;
