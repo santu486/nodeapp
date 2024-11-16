4984b0894023e6b0a856d2f6e830501203eaf7b5
santu486 <39703300+santu486@users.noreply.github.com>
2024-11-16 19:53:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express from santoh updated hereeeeeeeeeeeeeeee66666666666' });
+  require('dotenv').config(); // Load .env file
+  const myVar = process.env.MY_VARIABLE;
+  console.log("hereeeeeeeeeeeeeeee   " + myVar); // Prints 'some_value'
+// server.js
+console.log(`Your port is ${process.env.PORT}`); // undefined
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your port is ${process.env.PORT}`); // 8626
+  res.render('index', { title: 'santossh Express from santoh  code updated hereeeeeeeeeeeeee' + myVar });
 });
 
 module.exports = router;
